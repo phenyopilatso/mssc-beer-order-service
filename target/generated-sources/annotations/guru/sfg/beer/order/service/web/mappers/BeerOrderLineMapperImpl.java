a3108735c2276e3b1114219fee8b62b2e3cdeb27
phpilatso <phenyopilatso@momentum.co.za>
2020-10-20 09:40:05 +0200

    Add DB init script.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-19T10:48:36+0200",
+    date = "2020-10-19T16:48:32+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.8 (N/A)"
 )
 @Component
