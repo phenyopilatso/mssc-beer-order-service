267a8804260fa76af702cf40565ecaceffc06be3
Phenyo Pilatso <phenyo.pilatso@momentum.co.za>
2020-11-03 11:59:52 +0200

    Handle Validate Order Response.

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-11-02T15:16:35+0200",
+    date = "2020-11-03T11:19:07+0200",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
