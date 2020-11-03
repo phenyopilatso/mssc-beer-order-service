10d483eb34db440411d85314d60b4c0690fc9661
Phenyo Pilatso <phenyo.pilatso@momentum.co.za>
2020-11-03 10:17:59 +0200

    Refactor beer states.

--- src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStatemachineConfig.java
+++ src/main/java/guru/sfg/beer/order/service/sm/BeerOrderStatemachineConfig.java
@@ -30,7 +30,7 @@ public class BeerOrderStatemachineConfig extends StateMachineConfigurerAdapter<B
     @Override
     public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
         transitions.withExternal()
-                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+                .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_PENDING)
                 .event(BeerOrderEventEnum.VALIDATE_ORDER)
                 //todo add validation action here
             .and().withExternal()
